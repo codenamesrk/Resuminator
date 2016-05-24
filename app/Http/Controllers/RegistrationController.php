004e5ac4de82e5ca7dfd2c139db3caf0653313fb
srijit777 <srijit777@gmail.com>
2016-05-24 19:21:24 +0530

    removed die dump

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -129,7 +129,7 @@ class RegistrationController extends Controller
 
     public function paymentResponse(Request $request)
     {        
-        dd($request);
+        
         $response = Itdprocess::response($request);
         
         if($response['paymentstatus'] == 'success')
